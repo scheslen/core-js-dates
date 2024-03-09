1df09bf44eeeec85c4a9dca9585726c15d2ec40d
scheslen <antgalin@mail.ru>
2024-03-09 12:32:26 +0300

    feat: solve tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -290,8 +290,11 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const nYear = date.getFullYear();
+  if (nYear % 400 === 0) return true;
+  if (nYear % 4 === 0 && nYear % 100 !== 0) return true;
+  return false;
 }
 
 module.exports = {
